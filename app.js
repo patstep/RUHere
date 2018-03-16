73b3d90741e50ff161317c27d1f765c73b2995e4
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-03-15 20:44:03 -0400

    started to incorporate sequelize, built the mysql db

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // DEPENDENCIES
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 
-
+// set up the express app
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+// models
+const db = require('./models');
+
+// set up express app to handle data parsing
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}));
+// parse application
+app.use(bodyParser.json());
 
 // SET UP VIEW ENGINE
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
@@ -14,7 +25,7 @@ app.set('view engine', 'handlebars');
 // Serve static content for the app from the "public" directory in the app directory
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 8080;
+
 
 // set up routes
 app.use('/auth', authRoutes);
@@ -24,14 +35,19 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-// 
-
-
-
-
-
-
-// LISTEN TO PORT
-app.listen(PORT, () => {
-	console.log(`App now listening at PORT: ${PORT}`);
+// syncing our sequelize models and then starting our express app
+db.sequelize.sync({force:true}).then(() => {
+	app.listen(PORT, () => {
+		console.log(`App now listening at PORT: ${PORT}`);
+	});
 });
+
+
+
+
+
+
+// // LISTEN TO PORT
+// app.listen(PORT, () => {
+// 	console.log(`App now listening at PORT: ${PORT}`);
+// });
