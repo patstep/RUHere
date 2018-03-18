3ab214a470f5071ccea2edeb2e5e35dd762ac3c9
Kyle Makinen <KyleMakinen@Kyles-MacBook-Pro.local>
2018-03-17 22:30:16 -0400

    data collection

--- README.md
+++
@@ -1,2 +0,0 @@
-# RUHere
-Project 2 by group 4: an RU Bootcamp-oriented social media and networking site.
--- schema.sql
+++
@@ -1,4 +0,0 @@
--- drops the ruhere_db if it exists currently --
-DROP DATABASE IF EXISTS ruhere_db;
--- Creates the "ruhere_db" database --
-CREATE DATABASE todolist;
