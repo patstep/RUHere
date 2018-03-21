fb6def4afbed47c001a178c96f1ca92a54603ce4
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-03-20 21:10:09 -0400

    setting up socket.io chat function

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,8 +10,30 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        	//passport call back function
-        	console.log('it works');
-        	console.log(profile);
-    	})
-    );
+        //passport call back function
+        // console.log('it works');
+        // console.log(profile);
+        // check if user exists in DB
+        User.findOne({
+            where: {
+                googleID: profile.id
+
+            }
+        }).then((currentUser) => {
+            if (currentUser) {
+                // already have the user
+                console.log(`User is: ${currentUser}`);
+
+            } else {
+                // create new user
+                User.create({
+                    username: profile.displayName,
+                    gender: profile.gender,
+                    googleID: profile.id
+                    // console.log('new user created');
+                })
+            }
+        });
+
+    })
+)
